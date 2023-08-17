95b6a94ffad5a0a7f0d306d9970226c618f94a3e
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2023-08-17 22:27:26 +0300

    added from batch file

--- approutes.py
+++ approutes.py
@@ -7,8 +7,6 @@ import json
 
 approutes = Blueprint('approutes',__name__)
 
-'''
-
 
 def convert_list_to_json(inputlist):
     returnedjson = []
@@ -27,6 +25,27 @@ def get_users_data():
     userlistjson=convert_list_to_json(userlist)
     return (userlistjson)
 
+def get_types_data():
+    facturationtypels = db.engine.execute("""Select *,'Facturation' as 'Name' from facturationtype""")
+    facturationtypejson = convert_list_to_json(facturationtypels)
+
+    paymenttypels = db.engine.execute("""Select *, 'Paiement' as 'Name' from paymenttype""")
+    paymenttypejson = convert_list_to_json(paymenttypels)
+
+    retrocessiontypels = db.engine.execute("""Select *,'Retrocession' as 'Name' from retrocessiontype""")
+    retrocessiontypejson = convert_list_to_json(retrocessiontypels)
+
+    dentisterietypels = db.engine.execute("""Select *,'Dentisterie' as 'Name' from dentisterietype""")
+    dentisterietypejson = convert_list_to_json(dentisterietypels)
+
+    return (facturationtypejson+paymenttypejson+retrocessiontypejson+dentisterietypejson)
+    
+
+@approutes.route('/get_types_data')
+@login_required
+def gettypesdata():
+    return jsonify(get_types_data())
+
 @approutes.route('/get_users_data')
 @login_required
 def getusersdata():   
@@ -42,4 +61,22 @@ def delete_users():
             db.engine.execute("""Delete from "user" where id = {0}""".format(id))
     except:
         return(jsonify({"Status":"Not all records were deleted"}))    
-    return(jsonify({"Status":"OK"}))'''
+    return(jsonify({"Status":"OK"}))    
+
+@approutes.route('/edit_user',methods=["POST"])
+@login_required
+def edit_user():
+    
+    try:    
+        newaccess = ' '.join(request.json["access"])
+        
+        newrole = "admin" if request.json["isAdmin"]==True else "user"
+        
+        db.engine.execute("""UPDATE "user"
+        SET username = '{0}', password = '{1}', role='{2}',access='{3}'
+        WHERE id={4}; """.format(request.json["username"],request.json["password"],newrole,newaccess,request.json["id"]))
+    except:
+        return(jsonify({"Status":"Could not edit record"}))        
+   
+    return(jsonify({"Status":"OK"}))    
+    
