bdafe49e88051a3728d05ccd1aa6ff524291ec08
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2023-11-02 17:47:12 +0200

    added from batch file

--- static/js/qorpus/editemployeefunctions.js
+++ static/js/qorpus/editemployeefunctions.js
@@ -4,64 +4,81 @@ var url = new URL(curr_urll);
 var id = url.searchParams.get("id");
 
 var percentage_activities=[]
-var medsalaire_edit=[]
+var empsalaire_edit=[]
 
-function bulk_delete_medicins_perc_TMP_4edit()
+function bulk_delete_employee_perc_TMP_4edit()
 {
     var array = []
     var checkboxes = document.querySelectorAll('input[name=selectrowact]:checked')
     
     for (var i = 0; i < checkboxes.length; i++) {
-        delete_medicins_perc_TMP_4edit(checkboxes[i].value)
+        delete_employee_perc_TMP_4edit(checkboxes[i].value)
     }
 }
 
 
-function delete_medicins_perc_TMP_4edit(id)
+function delete_employee_perc_TMP_4edit(id)
 {
     var row = $(`#${id}`).closest('tr');
     
     let table = $('#responsiveDataTable').DataTable();
 
     for (var i=0;i<percentage_activities.length;i++){
-        if (percentage_activities[i][3]==id){
+        if (percentage_activities[i][5]==id){
             percentage_activities.splice(i, 1);
         }
     }
     
     table.row(row).remove().draw(false);
-    //alert(percentage_activities)
+    alert(percentage_activities)
     
 }
 
-function delete_medicins_salaire_TMP_4edit(id)
+function delete_employee_salaire_TMP_4edit(id)
 {
     var row = $(`#${id}`).closest('tr');
     
     let table = $('#responsiveDataTable2').DataTable();
 
-    for (var i=0;i<medsalaire_edit.length;i++){
-        if (medsalaire_edit[i][4]==id){
-            medsalaire_edit.splice(i, 1);
+    for (var i=0;i<empsalaire_edit.length;i++){
+        if (empsalaire_edit[i][4]==id){
+            empsalaire_edit.splice(i, 1);
         }
     }
     
     table.row(row).remove().draw(false);
     //alert(percentage_activities)
+    alert(empsalaire_edit)
     
 }
 
-function activate_deactivate_medsalaire(id){
+function activate_deactivate_empsalaire(id){
     var row = $(`#${id}`).closest('tr');
     
     let table = $('#responsiveDataTable2').DataTable();
     let updated_date = document.getElementById(`addDatePicker${id}`).value;
-    for (var i=0;i<medsalaire_edit.length;i++){
-        if (medsalaire_edit[i][4]==id){
-            medsalaire_edit[i][3]=updated_date
+    for (var i=0;i<empsalaire_edit.length;i++){
+        if (empsalaire_edit[i][4]==id){
+            empsalaire_edit[i][3]=updated_date
         }
     }
-    table.cell( row ,5).data( updated_date ).draw( false );    
+    table.cell( row ,4).data( updated_date ).draw( false );    
+    //alert(empsalaire_edit)
+
+}
+
+function activate_deactivate_empperc(id){
+    var row = $(`#${id}`).closest('tr');
+    
+    let table = $('#responsiveDataTable').DataTable();
+    let updated_date = document.getElementById(`addDatePicker${id}perc`).value;
+    for (var i=0;i<percentage_activities.length;i++){
+        if (percentage_activities[i][5]==id){
+            percentage_activities[i][4]=updated_date
+        }
+    }
+    table.cell( row ,6).data( updated_date ).draw( false );    
+    alert(percentage_activities)
 
 }
 
@@ -91,7 +108,7 @@ function add_item_to_percentage_table_4edit(){
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                     <!-- deleterow is important -->
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_medicins_perc_TMP_4edit(${rowUID})">Delete</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_employee_perc_TMP_4edit(${rowUID})">Delete</button>
                 </div>
             </div>
         </div>
@@ -132,7 +149,7 @@ function add_item_to_salaire_table_4edit(){
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="activate_deactivate_medsalaire(${rowUID})">Confirmer</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="activate_deactivate_empsalaire(${rowUID})">Confirmer</button>
             </div>
         </div>
     </div>
@@ -151,7 +168,7 @@ function add_item_to_salaire_table_4edit(){
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                     <!-- deleterow is important -->
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_medicins_salaire_TMP_4edit(${rowUID})">Delete</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_employee_salaire_TMP_4edit(${rowUID})">Delete</button>
                 </div>
             </div>
         </div>
@@ -159,7 +176,7 @@ function add_item_to_salaire_table_4edit(){
 
 
 </div>`
-medsalaire_edit.push([salairee,monthnumbers,fromdate,todate,rowUID])
+empsalaire_edit.push([salairee,monthnumbers,fromdate,todate,rowUID])
 table.row.add(["",row_functions,salairee,monthnumbers,fromdate,todate]).node().id = rowUID;
 table.draw(false);
 }
@@ -228,7 +245,7 @@ function modify_doctor(){
       "medstartdate":doctor_date_debut,
       "isemployee":doctor_isemp,
       "percentage_activities":percentage_activities,
-      "medsalaires":medsalaire_edit
+      "empsalaires":empsalaire_edit
 
       //"percentage_activities_for_current_doctor":percentage_activities_for_current_doctor
 
@@ -242,7 +259,7 @@ function modify_doctor(){
       }) 
   }
   else {
-      $("#editdoctorform")[0].reportValidity();
+      $("#editemployeeform")[0].reportValidity();
   }  
 
   
@@ -289,6 +306,26 @@ function load_employee_dataa(){
         let row_functions=`<div class="hstack gap-2 fs-15">
         <!-- duplicaterow2 and duplicaterow is important -->
         <!--<a aria-label="anchor" href="javascript:void(0);" class="btn btn-icon waves-effect waves-light btn-sm btn-secondary-light duplicaterow"><i class="ri-file-copy-line"></i></a>-->
+        <a aria-label="anchor" href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#valideDoctors${perc_act[i]["0"]}" class="btn btn-icon waves-effect waves-light btn-sm btn-warning-light"><i class="bi bi-power" data-bs-toggle="tooltip" data-bs-placement="top" title="activé/désactivé"></i></a>
+        <div class="modal fade mt-4" id="valideDoctors${perc_act[i]["0"]}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog  modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h6 class="modal-title" id="staticBackdropLabel">Désactivation</h6>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="product-name-add" class="form-label">Date de Désactivation:</label>
+                    <input type="date" id="addDatePicker${perc_act[i]["0"]}perc" value="" class="form-control text-muted" />
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="activate_deactivate_empperc(${perc_act[i]["0"]})">Confirmer</button>
+                </div>
+            </div>
+        </div>
+        </div>
+        
         <a aria-label="anchor" href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#deletepourcentageshare${perc_act[i]["0"]}" class="btn btn-icon waves-effect waves-light btn-sm btn-danger-light"><i class="ri-delete-bin-line"></i></a>
         <div class="modal fade mt-4" id="deletepourcentageshare${perc_act[i]["0"]}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -303,7 +340,7 @@ function load_employee_dataa(){
                     <div class="modal-footer">
                         <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                         <!-- deleterow is important -->
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_medicins_perc_TMP_4edit(${perc_act[i]["0"]})">Delete</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_employee_perc_TMP_4edit(${perc_act[i]["0"]})">Delete</button>
                     </div>
                 </div>
             </div>
@@ -311,8 +348,13 @@ function load_employee_dataa(){
     
     </div>`
 
-        percentage_activities.push([perc_act[i]["1"],perc_act[i]["2"],perc_act[i]["3"],perc_act[i]["4"],perc_act[i]["5"],perc_act[i]["0"]])
-        table.row.add([row_checkbox,row_functions,perc_act[i]["1"],perc_act[i]["2"],perc_act[i]["3"],perc_act[i]["4"],perc_act[i]["5"],perc_act[i]["6"]]).node().id = perc_act[i]["0"];
+        perc_date_fin=""
+        if(perc_act[i]["5"]!='1900-01-01'){
+            perc_date_fin=perc_act[i]["5"];
+        }
+
+        percentage_activities.push([perc_act[i]["1"],perc_act[i]["2"],perc_act[i]["3"],perc_act[i]["4"],perc_date_fin,perc_act[i]["0"]])
+        table.row.add([row_checkbox,row_functions,perc_act[i]["1"],perc_act[i]["2"],perc_act[i]["3"],perc_act[i]["4"],perc_date_fin,perc_act[i]["6"]]).node().id = perc_act[i]["0"];
         table.draw();
       }
 
@@ -339,7 +381,7 @@ function load_employee_dataa(){
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fermer</button>
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="activate_deactivate_medsalaire(${sal[i]["0"]})">Confirmer</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="activate_deactivate_empsalaire(${sal[i]["0"]})">Confirmer</button>
                 </div>
             </div>
         </div>
@@ -358,16 +400,18 @@ function load_employee_dataa(){
                     <div class="modal-footer">
                         <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                         <!-- deleterow is important -->
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_medicins_salaire_TMP_4edit(${sal[i]["0"]})">Delete</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="delete_employee_salaire_TMP_4edit(${sal[i]["0"]})">Delete</button>
                     </div>
                 </div>
             </div>
         </div>
     
     </div>`
-
-        medsalaire_edit.push([sal[i]["1"],sal[i]["2"],sal[i]["3"],sal[i]["4"],sal[i]["0"]])
-        tablesalaire.row.add(["",row_functions,sal[i]["1"],sal[i]["2"],sal[i]["3"],sal[i]["4"]]).node().id = sal[i]["0"];
+    let datefin=""    
+    if (sal[i][4]!='1900-01-01')
+        {datefin=sal[i][4]}
+        empsalaire_edit.push([sal[i]["1"],sal[i]["2"],sal[i]["3"],datefin,sal[i]["0"]])
+        tablesalaire.row.add([row_functions,sal[i]["1"],sal[i]["2"],sal[i]["3"],datefin]).node().id = sal[i]["0"];
         tablesalaire.draw();
       }
       
